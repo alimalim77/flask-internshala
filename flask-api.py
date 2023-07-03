361eeff2854ea13e2cf187c3a0b575e430e9d9f3
alimalim77 <alimalim77@gmail.com>
2023-07-04 01:22:59 +0530

    first commit

---
+++ flask-api.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__, template_folder='template')
+
+class Fruit:
+    def __init__(self, id, name, color):
+        self.id = id
+        self.name = name
+        self.color = color
+
+def sort_fruits_by_color(fruits):
+    sorted_fruits = sorted(fruits, key=lambda fruit: fruit.color)
+    return sorted_fruits
+
+fruits = [] 
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/fruits', methods=['GET', 'POST'])
+def handle_fruits():
+    if request.method == 'POST':
+        fruit_name = request.form.get('fruit')
+        fruit_color = request.form.get('color')
+        new_fruit = Fruit(len(fruits) + 1, fruit_name, fruit_color)
+        fruits.append(new_fruit)
+        sorted_fruits = sort_fruits_by_color(fruits)
+        fruit_list = []
+        for fruit in sorted_fruits:
+            fruit_list.append({
+                'id': fruit.id,
+                'name': fruit.name,
+                'color': fruit.color
+            })
+        print(fruit_list)
+        return jsonify(fruit_list)
+    elif request.method == 'GET':
+        return jsonify(fruits)
+
+if __name__ == '__main__':
+    app.run()
